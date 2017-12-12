e9f81ddcc94194cfe7e410e1fffea36222b8d8d8
Cliff Chen <cliff.chen@rock-chips.com>
2017-12-12 17:00:57 +0800

    fix compile error in O
    google change some api of bionic since oreo, so fix for compile
    
    Change-Id: Ie3affc744481b2a6bc00711f42980b702b9b5b78
    Signed-off-by: Cliff Chen <cliff.chen@rock-chips.com>

--- libfuse-lite/Android.mk
+++ libfuse-lite/Android.mk
@@ -3,19 +3,16 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
 	fuse.c 			\
-	fuse_i.h 		\
 	fuse_kern_chan.c 	\
 	fuse_loop.c 		\
 	fuse_lowlevel.c 	\
-	fuse_misc.h 		\
 	fuse_opt.c 		\
 	fuse_session.c 		\
 	fuse_signals.c 		\
 	fusermount.c		\
 	helper.c 		\
 	mount.c 		\
-	mount_util.c 		\
-	mount_util.h
+	mount_util.c 		
 
 LOCAL_MODULE := libfuse-lite
 LOCAL_MODULE_TAGS := optional
